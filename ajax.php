27c7b4887028e29b5216bfa8ada3544c1d76059b
luu chan nam <naml45889@gmail.com>
2020-03-18 16:21:07 +0700

    them file

---
+++ ajax.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<h1>AJAX</h1>
+    <p>AJAX is not a programming language.</p>
+    <p>AJAX is a technique for accessing web servers from a web page.</p>
+    <p>AJAX stands for Asynchronous JavaScript And XML.</p>';
+?>
